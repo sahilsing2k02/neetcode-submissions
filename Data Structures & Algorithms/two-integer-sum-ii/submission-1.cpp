847d4217b5cdc155be598650cf83bb3642ef1e9e
Sahil Singh <149385923+sahilsing2k02@users.noreply.github.com>
2026-08-19 10:19:07 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l =0, r= numbers.size()-1;
+
+        while(l<r){
+            int cur = numbers[l]+numbers[r];
+            if(cur>target)r--;
+            else if(cur<target)l++;
+            else
+            return {l+1,r+1};
+        }
+        return{};
+    }
+};
